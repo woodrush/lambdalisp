f8c043371a2b85e417dfcc2db05b72cc7b7a4e42
Hikaru Ikuta <woodrush924@gmail.com>
2022-08-13 12:19:13 +0900

    Support Iota syntax

--- lazyk-to-blc.sh
+++ lazyk-to-blc.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 set -e
 
-wrapper=$1
-file=$2
+
+file=$1
+wrapper=$2
+iotasyntax=$3
+
+if [ -z "$wrapper" ]; then
+    wrapper="./wrapper.blc"
+fi
+
 
 code=$(cat $file | tr -d "\n")
 
-code=$(printf $code | sed s/\`/01/g)
-code=$(printf $code | sed s/s/00000001011110100111010/g)
-code=$(printf $code | sed s/k/0000110/g)
-code=$(printf $code | sed s/i/0010/g)
+if [ ! -z "$iotasyntax" ]; then
+    code=$(printf $code | sed s/\*/01/g)
+    code=$(printf $code | sed s/i/00010110000000010111101001110100000110/g)
+else
+    code=$(printf $code | sed s/\`/01/g)
+    code=$(printf $code | sed s/s/00000001011110100111010/g)
+    code=$(printf $code | sed s/k/0000110/g)
+    code=$(printf $code | sed s/i/0010/g)
+fi
 
 sed -f - $wrapper << EOF
 s/\\[program\\]/$code/g
