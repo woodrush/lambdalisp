f2fb339053aa6dd980d7a51c577615f79d70ad3c
Hikaru Ikuta <woodrush924@gmail.com>
2022-09-03 17:00:38 +0900

    Update rest.lisp with ,@

--- rest.lisp
+++ rest.lisp
@@ -1,10 +1,8 @@
 (defvar cond (macro (a &rest b)
   (if b
-    (cons (quote if)
-    (cons (car a)
-    (cons (car (cdr a))
-    (cons (cons (quote cond) b)
-    ()))))
+    `(if ,(car a)
+      ,(car (cdr a))
+      (cond ,@b))
     (car (cdr a)))))
 
 (cond
@@ -14,4 +12,3 @@
 (cond
   (() (quote a))
   ((quote c) (quote b)))
-
