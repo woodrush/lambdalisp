8fcb5acd3cab65c4f41ca88119c1928e631e2969
Hikaru Ikuta <woodrush924@gmail.com>
2022-09-04 07:25:08 +0900

    Extend reader macro example

--- reader-macro.lisp
+++ reader-macro.lisp
@@ -1,6 +1,11 @@
 (defvar my-quote (lambda (char)
   `',(read)))
-
 (set-macro-character "#" my-quote)
 
-(print #(a b c))
+(print (append #(a b c) #(d e f)))
+
+(defvar cdr-quote (lambda (char)
+  `',(cdr (read))))
+(set-macro-character "!" cdr-quote)
+
+(print (append !(a b c) !(d e f)))
