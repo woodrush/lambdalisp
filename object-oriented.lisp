611bf9d6c27d4e4a237ecbacc75aa95ea67859c5
Hikaru Ikuta <woodrush924@gmail.com>
2022-09-04 11:23:53 +0900

    Add getter and setter

--- object-oriented.lisp
+++ object-oriented.lisp
@@ -8,15 +8,25 @@
   `(,instance ',accesor))
 
 (defmacro new (&rest args)
-  args)
+  `((. (,(car args)) __init__) ,@(cdr args)))
 
-(defmacro build-accessor (args)
+(defmacro build-getter (args)
   (defun helper (args)
     (if args
-      `(if (eq a ',(car args)) ,(car args)
-        ,(helper (cdr args)))
+      `(if (eq a ',(car args))
+          ,(car args)
+          ,(helper (cdr args)))
       nil))
   `(lambda (a) ,(helper args)))
+
+(defmacro build-setter (args)
+  (defun helper (args)
+    (if args
+      `(if (eq key ',(car args))
+          (setq ,(car args) value)
+          ,(helper (cdr args)))
+      nil))
+  `(lambda (key value) ,(helper args)))
 
 (defmacro let* (binding &rest body)
   (defun helper (args)
@@ -26,23 +36,34 @@
   (helper binding))
 
 (defmacro defclass (name &rest body)
-  (defun helper (args)
+  (defun collect-fieldnames (args)
     (if args
-      (cons (car (car args)) (helper (cdr args))
+      (cons (car (car args)) (collect-fieldnames (cdr args))
       nil)))
+  (setq fieldnames (collect-fieldnames body))
   `(defun ,name ()
-      (let* (
-        (self ())
-        ,@body)
-        (setq self (build-accessor ,(helper body))))))
+      (let* ((self ())
+             (setter ())
+              ,@body)
+        (setq setter (build-setter ,fieldnames))
+        (setq self (build-getter ,fieldnames)))))
+
+(defmacro setfield (name value)
+  `(setter ',name ,value))
 
 (defclass counter
   (i ())
-  (c (quote a))
+  (c ())
   (inc (lambda ()
-    (setq i (cons c i))))
+    (setfield i (cons c (. self i)))))
   (dec (lambda ()
-    (if i (setq i (cdr i)) i))))
+    (if i
+      (setfield i (cdr i))
+      i)))
+  (__init__ (lambda (c)
+    (setfield c c)
+    self)))
+
 
 (defvar counter1 (new counter (quote a)))
 (defvar counter2 (new counter (quote b)))
