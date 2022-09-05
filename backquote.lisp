95b03b3152f33215f0886be268a717b2be010561
Hikaru Ikuta <woodrush924@gmail.com>
2022-09-05 22:25:15 +0900

    WIP: backquote as a user-defined reader macro

--- backquote.lisp
+++ backquote.lisp
@@ -1,36 +1,52 @@
-(defun backquote-function (expr)
-  (block backquote
-    (cond
-      ((eq (type expr) 'atom)
-        expr)
-      ((eq (type expr) 'cons)
-        (cond
-          ((and (eq 'cons (type (car expr)))
-                (eq 'comma-splice (car (car expr))))
-            (append (eval (car (cdr (car expr)))) (backquote-function (cdr expr))))
-          ((eq (car expr) 'comma)
-            (eval (car (cdr expr))))
-          (t
-            (cons (backquote-function (car expr)) (backquote-function (cdr expr)))))))))
+(defvar and (lambda (x y) (if x y nil)))
 
-(defun backquote-macro (char)
+
+(defvar list (macro (&rest y)
+  (if y
+    (cons 'cons (cons (car y) (cons (cons 'list (cdr y)) nil)))
+    nil)))
+(list 'a 'b 'c)
+(defvar cond (macro (a &rest b)
+  (if a
+    (list 'if (car a)
+      (cons 'progn (cdr a))
+      (cons 'cond b))
+    nil)))
+
+(cond (t t) (nil nil))
+
+(defvar backquote-function (lambda (expr)
+  (cond
+    ((eq (type expr) 'atom)
+      expr)
+    ((eq (type expr) 'cons)
+      (print expr)
+      (cond
+        ((and (eq 'cons (type (car expr)))
+              (eq 'comma-splice (car (car expr))))
+          (append (eval (car (cdr (car expr)))) (backquote-function (cdr expr))))
+        ((eq (car expr) 'comma)
+          (eval (car (cdr expr))))
+        (t
+          (cons (backquote-function (car expr)) (backquote-function (cdr expr)))))))))
+
+(defvar backquote-macro (lambda (char)
   (setq expr (read))
-  (print expr)
-  (cons 'backquote-function (cons (cons 'quote (cons expr nil)) nil)))
-(set-macro-character "!" backquote-macro)
+  (cons 'backquote-function (cons (cons 'quote (cons expr nil)) nil))))
+(set-macro-character "`" backquote-macro)
 
-(defun comma-macro (char)
-  (if (eq (peek-char) "*")
+(defvar comma-macro (lambda (char)
+  (if (eq (peek-char) "@")
     (progn
       (read-char)
       (setq expr (read))
       (cons 'comma-splice (cons expr nil)))
     (progn
       (setq expr (read))
-      (cons 'comma (cons expr nil)))))
-(set-macro-character "_" comma-macro)
+      (cons 'comma (cons expr nil))))))
+(set-macro-character "," comma-macro)
 
 
 (setq bbb '(b b b))
 (setq xyz '(x y z))
-!(a _*bbb _xyz c)
+`(a ,@bbb ,xyz c)
