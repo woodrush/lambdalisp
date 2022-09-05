a521b127be293e8e94c87bc63f1c38c85a607fa3
Hikaru Ikuta <woodrush924@gmail.com>
2022-09-05 17:56:40 +0900

    Implement backquote as a reader macro

--- backquote.lisp
+++ backquote.lisp
@@ -1,4 +1,5 @@
-(defun backquote (expr)
+(defun backquote-function (expr)
+  (print expr)
   (block backquote
     (cond
       ((eq (type expr) 'atom)
@@ -7,11 +8,28 @@
         (cond
           ((and (eq 'cons (type (car expr)))
                 (eq 'comma-splice (car (car expr))))
-            (append (eval (car (cdr (car expr)))) (backquote (cdr expr))))
+            (append (eval (car (cdr (car expr)))) (backquote-function (cdr expr))))
           ((eq (car expr) 'comma)
             (print (eval (car (cdr expr)))))
           (t
-            (cons (backquote (car expr)) (backquote (cdr expr)))))))))
+            (cons (backquote-function (car expr)) (backquote-function (cdr expr)))))))))
+
+(defun backquote-macro (char)
+  (setq expr (read))
+  (cons 'backquote-function (cons (cons 'quote (cons expr nil)) nil)))
+(set-macro-character "`" backquote-macro)
+
+(defun comma-macro (char)
+  (if (eq (peek-char) "@")
+    (progn
+      (read-char)
+      (setq expr (read))
+      (cons 'comma-splice (cons expr nil)))
+    (progn
+      (setq expr (read))
+      (cons 'comma (cons expr nil)))))
+(set-macro-character "," comma-macro)
+
 
 (setq aaa '(b b b))
-(backquote '(a (comma-splice aaa) c))
+`(a ,@aaa c)
