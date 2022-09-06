a2ed3d1c3ec7369f9dc5f2d49548702fa15f7473
Hikaru Ikuta <woodrush924@gmail.com>
2022-09-06 16:24:29 +0900

    Refine reader macro example with initiator character branching

--- reader-macro.lisp
+++ reader-macro.lisp
@@ -14,28 +14,29 @@
 
 
 (defvar reader-list (lambda (char)
-  (let ((ret ()) (token ()) (helper ()))
+  (block reader-list
+    (if (eq char "]")
+      (return-from reader-list '**reader-list-end**)
+      nil)
     (setq helper (lambda (l)
-      (let ((helper-ret ()) (l l))
-        (loop
-          (if (atom l)
-            (return-from () helper-ret)
-            nil)
-          (setq helper-ret (cons 'cons (cons (car l) (cons helper-ret nil))))
-          (setq l (cdr l))))))
+      (setq helper-ret ())
+      (loop
+        (if (atom l)
+          (return-from () helper-ret)
+          nil)
+        (setq helper-ret (cons 'cons (cons (car l) (cons helper-ret nil))))
+        (setq l (cdr l)))))
+    (setq ret ())
     (loop
-      (if (eq (peek-char) "]")
-        (progn
-          (read-char)
-          (return-from () (helper ret)))
-        nil)
       (setq token (read))
-      (if (eq token '])
+      (if (eq token '**reader-list-end**)
         (return-from () (helper ret))
         nil)
       (setq ret (cons token ret))))))
 (set-macro-character "[" reader-list)
+(set-macro-character "]" reader-list)
+
 
 (print [1 2 3 4 5])
-(print [1 [2 3 ] 4 [5 6 ] ])
+(print [1 [(cons 3 (cons 4 nil)) (+ 2 3) (* 2 3)] 4 [5 6 ] ])
 (print (append [1 2 [3 [4 5] 6] 7 8] [9 10 [[11 12] [13 14]] 15 16]))
